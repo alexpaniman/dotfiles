fd941cfae45743f6f2db45ab9242559fab4df189
Alexander Paniman <alexpaniman@gmail.com>
2020-06-27 20:26:09 +0300

    Change key bindings to keep fingers on a home row

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -15,6 +15,9 @@ import XMonad.Layout.MultiToggle
 import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Layout.MultiToggle.Instances
 
+import XMonad.Actions.CopyWindow (kill1)
+import XMonad.Actions.WithAll (killAll)
+
 import XMonad.Util.NamedScratchpad
 import qualified XMonad.StackSet as W
 
@@ -24,6 +27,8 @@ myTerminal = "alacritty"
 myMusicPlayer = "cmus"
 myTop = "gotop"
 myBrowser = "brave"
+myFileManager = "vifm"
+myEditor = "nvim"
 
 myStartupHook = do
     spawnOnce "nitrogen --restore &"
@@ -70,15 +75,19 @@ main = xmonad $ desktopConfig
     -- XMonad
       ("M-S-r", spawn "xmonad --restart")
     , ("M-S-q", io exitSuccess)
+    -- Terminal
+    , ("M-<Return>", spawn $ myTerminal)
     -- Apps
-    , ("M-M1-b", spawn myBrowser)
-    , ("M-M1-t", spawn $ myTerminal ++ " -e " ++ myTop)
+    , ("M-; i", spawn myBrowser)
+    , ("M-; t", spawn $ myTerminal ++ " -e " ++ myTop)
+    , ("M-; e", spawn $ myTerminal ++ " -e " ++ myEditor)
+    , ("M-; f", spawn $ myTerminal ++ " -e " ++ myFileManager)
     -- Fullscreen
-    , ("M-S-f", sendMessage $ Toggle FULL)
+    , ("M-f", sendMessage $ Toggle FULL)
     -- Mirror
-    , ("M-S-\\", sendMessage $ Toggle MIRROR)
+    , ("M-m", sendMessage $ Toggle MIRROR)
     -- DMenu
-    , ("M-<Return>", spawn $ "dmenu_run -fn 'Fira Code Medium:size=12'" ++
+    , ("M-o", spawn $ "dmenu_run -fn 'Fira Code Medium:size=12'" ++
                    " -nb '#282828'" ++
 				   " -nf '#cccccc'" ++
 				   " -sb '#d79921'" ++
@@ -88,17 +97,20 @@ main = xmonad $ desktopConfig
     , ("M-<Up>", spawn "amixer set Master 5%+")
     , ("M-<Down>", spawn "amixer set Master 5%-")
     -- Scratchpads
-    , ("M-C-<Return>", namedScratchpadAction myScratchpads "terminal")
-    , ("M-C-m", namedScratchpadAction myScratchpads "music-player")
+    , ("M-s <Return>", namedScratchpadAction myScratchpads "terminal")
+    , ("M-s m", namedScratchpadAction myScratchpads "music-player")
+    -- Close Windows
+    , ("M-d", kill1)
+    , ("M-S-d", killAll)
     -- Screenshot
     , ("C-<Print>", spawn "maim -s | xclip -selection clipboard -t image/png")
     , ("<Print>", spawn "maim | xclip -selection clipboard -t image/png")
     -- Music
-    , ("M-m <Space>", spawn "cmus-remote --pause")
-    , ("M-m n", spawn "cmus-remote --next")
-    , ("M-m p", spawn "cmus-remote --prev")
-    , ("M-m r", spawn "cmus-remote --repeat")
-    , ("M-m s", spawn "cmus-remote --shuffle")
-    , ("M-m h", spawn "cmus-remote --seek -5")
-    , ("M-m l", spawn "cmus-remote --seek +5")
+    , ("M-p <Space>", spawn "cmus-remote --pause")
+    , ("M-p n", spawn "cmus-remote --next")
+    , ("M-p p", spawn "cmus-remote --prev")
+    , ("M-p r", spawn "cmus-remote --repeat")
+    , ("M-p s", spawn "cmus-remote --shuffle")
+    , ("M-p h", spawn "cmus-remote --seek -15")
+    , ("M-p l", spawn "cmus-remote --seek +15")
     ]
