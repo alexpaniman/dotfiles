9ad3f21a2d93028f1d08af8f92cd51a2c370dc35
Alexander Paniman <alexpaniman@gmail.com>
2020-08-10 15:27:52 +0300

    Configuring org mode in doom emacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -15,7 +15,7 @@
       user-mail-address "alexpaniman@gmail.com")
 
 ;; Set theme. It's similar to doom-one
-(setq doom-theme 'doom-vibrant)
+(setq doom-theme 'doom-monokai-pro)
 
 ;; Directory for org files
 (setq org-directory "~/org/")
@@ -25,17 +25,49 @@
       doom--line-number-style 'relative)
 
 ;; Change doom logo to black hole
-(setq fancy-splash-image "~/.doom.d/black-hole.png")
+;; (setq fancy-splash-image "~/.doom.d/black-hole.png")
 
 (defun fira-code (size)
   (font-spec :family "Fira Code"
              :size size))
 
 ;; Set fonts
-(setq doom-font (fira-code 18)
-      doom-big-font (fira-code 24)
+(setq doom-font (fira-code 16)
+      doom-big-font (fira-code 22)
       doom-variable-pitch-font (fira-code 18))
 
 ;; Path that projectile uses to discover projects
 (setq projectile-project-search-path
       '("~/projects/"))
+
+;; ---> Org mode <---
+
+;; Enable org-mode to export org file with cyrillic to pdf
+(setq org-latex-packages-alist
+      '(("utf8" "inputenc" t)
+        ("T2A" "fontenc" t)
+        ("russian,english" "babel" t)))
+
+;; Hide org-mode markers like *bold* or ~code~
+(setq org-hide-emphasis-markers t)
+
+;; Make LaTeX previews in org-mode bigger
+(setq org-format-latex-options
+      (plist-put org-format-latex-options :scale 1.5))
+
+;; Enable graphviz dot support inside org-mode source block
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((dot . t)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (rainbow-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
