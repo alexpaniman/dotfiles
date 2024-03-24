885466c62043296e82b90d426a1d3cf7e6ee305d
alexpaniman <alexpaniman@gmail.com>
2024-03-24 17:43:18 +0300

    refactor: remove unused xmonad features since i moved to pc on nixos

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,15 +3,13 @@ import XMonad
 import XMonad.Config.Desktop
 import System.Exit (exitSuccess)
 
-import XMonad.Layout.IndependentScreens (withScreens)
-
 import XMonad.Layout.Spacing (spacing)
 
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout.ResizableTile
 
 import XMonad.Util.SpawnOnce (spawnOnce)
-import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.EZConfig
 
 import XMonad.Layout.MultiToggle as MT
 import XMonad.Layout.ToggleLayouts as TL
@@ -30,6 +28,10 @@ import qualified XMonad.StackSet as W
 import XMonad.Hooks.SetWMName (setWMName)
 import XMonad.Hooks.EwmhDesktops
 
+import XMonad.Actions.CycleWS
+import Data.Map
+  
+
 myTerminal = "alacritty"
 myMusicPlayer = "cmus"
 myTop = "gotop"
@@ -38,24 +40,6 @@ myFileManager = "/home/alex/.config/vifm/scripts/vifmrun"
 myEditor = "emacsclient -nc -a ''"
 
 myStartupHook = do
-    spawnOnce "/home/alex/.profile &"
-
-    spawnOnce "flameshot &"
-    
-    spawnOnce "nitrogen --restore &"
-
-    spawnOnce $ "picom" ++
-        " --experimental-backends" ++
-        " --backend glx" ++
-        " --vsync" ++
-        " --config ~/.config/picom/picom.conf &"
-
-    spawnOnce "/usr/lib/xfce4/notifyd/xfce4-notifyd &"
-    spawnOnce "udiskie &"
-
-    spawnOnce "emacs --daemon &"
-    spawnOnce "/home/alex/.local/bin/huion-setup.sh primary"
-
     setWMName "LG3D"
 
 myLayoutHook = toggleLayouts (noBorders Full) $ mkToggle (single MIRROR) $ smartBorders $ mkToggle (NOBORDERS ?? FULL ?? EOT) $ equalSpacing 24 6 0 6 $
@@ -76,6 +60,10 @@ myScratchpads = [
 
         manageScratchpad = customFloating $ W.RationalRect 0.05 0.05 0.9 0.9
 
+
+myWorkspaces = [show i | i <- [0..9]]
+  
+
 main = xmonad $ ewmh desktopConfig
     { borderWidth = 2
     , startupHook = myStartupHook
@@ -83,69 +71,83 @@ main = xmonad $ ewmh desktopConfig
     , layoutHook = myLayoutHook
     , modMask = mod4Mask
     , terminal = myTerminal
-    , workspaces = withScreens 2 [show i | i <- [0..9]]
+    , workspaces = myWorkspaces
     , focusFollowsMouse = False
     , normalBorderColor = "#18081A"
     , focusedBorderColor = "#fa0574" -- "#6f00cc"
     } `additionalKeysP` [
+
     -- XMonad
-      ("M-S-r", spawn "xmonad --restart")
-    , ("M-S-q", io exitSuccess)
+      ("M-S-r"            , spawn "xmonad --restart")
+    , ("M-S-q"            , io exitSuccess)
+
     -- Terminal
-    , ("M-<Return>", spawn $ myTerminal)
+    , ("M-<Return>"       , spawn $ myTerminal)
+
     -- Apps
-    , ("M-i", spawn $ myBrowser)
-    , ("M-a", spawn $ myEditor)
-    , ("M-s", spawn $ "~/.emacs_anywhere/bin/run")
-    , ("M-; t", spawn $ myTerminal ++ " -e " ++ myTop)
-    , ("M-; f", spawn $ myTerminal ++ " -e " ++ myFileManager)
+    , ("M-i"              , spawn $ myBrowser)
+    , ("M-a"              , spawn $ myEditor)
+
+    , ("M-S-s"            , shiftNextScreen >> nextScreen)
+    , ("M-s"              , nextScreen)
+
+    , ("M-; t"            , spawn $ myTerminal ++ " -e " ++ myTop)
+    , ("M-; f"            , spawn $ myTerminal ++ " -e " ++ myFileManager)
+
     -- Debug
-    , ("M-; e", spawn "ps aux | grep -ie emacs | grep -v grep | awk '{print $2}' | xargs kill -SIGUSR2")
+    , ("M-; e"            , spawn "ps aux | grep -ie emacs | grep -v grep | awk '{print $2}' | xargs kill -SIGUSR2")
+
     -- Fullscreen
-    , ("M-f", -- (withFocused toggleBorder) <+> (sendMessage $ Toggle FULL)
-       (sendMessage (TL.Toggle "Full"))
-      )
-    -- , ("M-b", withFocused toggleBorder)
+    , ("M-f"              , (sendMessage (TL.Toggle "Full")))
+
     -- Mirror
-    , ("M-m", sendMessage $ MT.Toggle MIRROR)
+    , ("M-m"              , sendMessage $ MT.Toggle MIRROR)
+
     -- DMenu
-    , ("M-o", spawn $ "dmenu_run -p 'Run anything:'")
+    , ("M-o"              , spawn $ "rofi -show run")
+
     -- Volume
-    , ("M-<Up>", spawn "amixer set Master 5%+")
-    , ("M-<Down>", spawn "amixer set Master 5%-")
+    , ("M-<Up>"           , spawn "pactl set-sink-volume @DEFAULT_SINK@ +5%")
+    , ("M-<Down>"         , spawn "pactl set-sink-volume @DEFAULT_SINK@ -5%")
+
     -- Scratchpads
-    , ("M-C-<Return>", namedScratchpadAction myScratchpads "terminal")
-    , ("M-C-m", namedScratchpadAction myScratchpads "music-player")
+    , ("M-C-<Return>"     , namedScratchpadAction myScratchpads "terminal")
+    , ("M-C-m"            , namedScratchpadAction myScratchpads "music-player")
+
     -- Close Windows
-    , ("M-d", kill1)
-    , ("M-S-d", killAll)
+    , ("M-d"              , kill1)
+    , ("M-S-d"            , killAll)
+
     -- Screenshot
-    , ("S-<Print>", spawn "flameshot gui")
-    , ("C-<Print>", spawn "maim -s | xclip -selection clipboard -t image/png")
-    , ("<Print>", spawn "maim | xclip -selection clipboard -t image/png")
-    -- Music
-    , ("M-p <Space>", spawn "cmus-remote --pause")
-    , ("M-p n", spawn "cmus-remote --next")
-    , ("M-p p", spawn "cmus-remote --prev")
-    , ("M-p r", spawn "cmus-remote --repeat")
-    , ("M-p s", spawn "cmus-remote --shuffle")
-    , ("M-p h", spawn "cmus-remote --seek -15")
-    , ("M-p l", spawn "cmus-remote --seek +15")
-    , ("M-C-<Up>", spawn "~/.local/bin/change-volume 'ALSA plug-in [cmus]' +5")
-    , ("M-C-<Down>", spawn "~/.local/bin/change-volume 'ALSA plug-in [cmus]' -5")
-    , ("M-v t <Up>", spawn "~/.local/bin/change-volume 'WEBRTC VoiceEngine' +15")
-    , ("M-v t <Down>", spawn "~/.local/bin/change-volume 'WEBRTC VoiceEngine' -15")
+    , ("S-<Print>"        , spawn "flameshot gui"                                    )
+    , ("C-<Print>"        , spawn "maim -s | xclip -selection clipboard -t image/png")
+    , ("<Print>"          , spawn "maim | xclip -selection clipboard -t image/png"   )
+
     -- Scripts
-    , ("M-x d", spawn "~/.local/bin/mount-android-device")
-    , ("M-]", spawn "~/.local/bin/dmenu_latex")
-    , ("M-[", spawn "~/.local/bin/read-screen")
-    , ("M-g", spawn "~/.local/bin/search-clipboard")
-    , ("M-S-g", spawn "~/.local/bin/search-selection")
-    , ("M-; m", spawn "~/.local/bin/spawn-mindustry")
-    -- , ("M-/", spawn "~/.local/bin/send-screenshot")
-    , ("M-'", sendMessage (TL.Toggle "Full"))
-    , ("M-<Left>", spawn "~/.local/bin/huion-setup.sh 1")
-    , ("M-<Right>", spawn "~/.local/bin/huion-setup.sh 2")
-    -- , ("M-<Left>", spawn "~/.local/bin/photo-previous")
-    -- , ("M-<Right>", spawn "~/.local/bin/photo-next")
+    , ("M-'"              , sendMessage (TL.Toggle "Full")                           )
     ]
+    ++
+    [ ("M-1"              , windows $ lazyView $ myWorkspaces !! 0)
+    , ("M-2"              , windows $ lazyView $ myWorkspaces !! 1)
+    , ("M-3"              , windows $ lazyView $ myWorkspaces !! 2)
+    , ("M-4"              , windows $ lazyView $ myWorkspaces !! 3)
+    , ("M-5"              , windows $ lazyView $ myWorkspaces !! 4)
+    , ("M-6"              , windows $ lazyView $ myWorkspaces !! 5)
+    , ("M-7"              , windows $ lazyView $ myWorkspaces !! 6)
+    , ("M-8"              , windows $ lazyView $ myWorkspaces !! 7)
+    , ("M-9"              , windows $ lazyView $ myWorkspaces !! 8)]
+    ++
+    [ ("M-S-1"            , windows $ W.shift  $ myWorkspaces !! 0)
+    , ("M-S-2"            , windows $ W.shift  $ myWorkspaces !! 1)
+    , ("M-S-3"            , windows $ W.shift  $ myWorkspaces !! 2)
+    , ("M-S-4"            , windows $ W.shift  $ myWorkspaces !! 3)
+    , ("M-S-5"            , windows $ W.shift  $ myWorkspaces !! 4)
+    , ("M-S-6"            , windows $ W.shift  $ myWorkspaces !! 5)
+    , ("M-S-7"            , windows $ W.shift  $ myWorkspaces !! 6)
+    , ("M-S-8"            , windows $ W.shift  $ myWorkspaces !! 7)
+    , ("M-S-9"            , windows $ W.shift  $ myWorkspaces !! 8)]
+
+
+
+isVisible w ws = any ((w ==) . W.tag . W.workspace) (W.visible ws)
+lazyView w ws = if isVisible w ws then ws else W.view w ws
